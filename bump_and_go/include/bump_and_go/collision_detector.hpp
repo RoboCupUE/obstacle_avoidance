9e1c6c4b528dea4775a9d220bc4b70ea45453461
fgonzalezr1998 <fergonzaramos@yahoo.es>
2024-11-17 23:00:23 +0100

    [collision_detector] WIP

--- bump_and_go/include/bump_and_go/collision_detector.hpp
+++ bump_and_go/include/bump_and_go/collision_detector.hpp
@@ -3,6 +3,8 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <laser_geometry/laser_geometry.hpp>
+#include "tf2_ros/static_transform_broadcaster.h"
 
 #include <string>
 #include <vector>
@@ -19,16 +21,25 @@ public:
   void step(void);
   double getRate(void);
 private:
+  void staticTransformBroadcast(void);
+  void publishCollisionData(void);
   void laserCallback(const LaserScan & msg);
   void initParams(void);
 
-  int angle2laserIndex(float rad, const LaserScan & scanMsg);
-
   rclcpp::Subscription<LaserScan>::SharedPtr laserSub_;
+  LaserScan scanMsg_;
+  tf2_ros::StaticTransformBroadcaster tf2StaticBroadcaster_;
 
   // Configuration parameters
   double rate_;
+  double collisionDist_;
   std::string laserTopic_;
+  std::vector<double> leftRangeY_;
+  std::vector<double> righRangeY_;
+  std::vector<double> frontRangeY_;
+  std::string fixedFrame_;
+  std::string childFrame_;
+  std::vector<double> transformCoords_;
 };
 } // end namespace collision_detector
 
--- bump_and_go/src/bump_and_go/collision_detector.cpp
+++ bump_and_go/src/bump_and_go/collision_detector.cpp
@@ -1,4 +1,5 @@
 #include "bump_and_go/collision_detector.hpp"
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 #include <math.h>
 #include <string>
@@ -8,7 +9,7 @@ using std::placeholders::_1;
 namespace collision_detector
 {
   CollisionDetector::CollisionDetector(const std::string & node_name)
-  : Node(node_name)
+  : Node(node_name), tf2StaticBroadcaster_(this)
   {
     initParams();
 
@@ -18,7 +19,8 @@ namespace collision_detector
 
   void CollisionDetector::step(void)
   {
-    ;
+    staticTransformBroadcast();
+    publishCollisionData();
   }
 
   double CollisionDetector::getRate(void)
@@ -30,43 +32,59 @@ namespace collision_detector
    * Private Methods *
    *******************/
 
+  void CollisionDetector::staticTransformBroadcast(void)
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = fixedFrame_;
+    t.child_frame_id = childFrame_;
+
+    t.transform.translation.x = transformCoords_.at(0);
+    t.transform.translation.y = transformCoords_.at(1);
+    t.transform.translation.z = transformCoords_.at(2);
+
+    tf2::Quaternion q;
+    q.setRPY(
+      transformCoords_.at(3),
+      transformCoords_.at(4),
+      transformCoords_.at(5));
+
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    tf2StaticBroadcaster_.sendTransform(t);
+  }
+
+  void CollisionDetector::publishCollisionData(void)
+  {
+    ;
+  }
+
   void CollisionDetector::laserCallback(const LaserScan & msg)
   {
+    scanMsg_ = msg;
     RCLCPP_INFO(this->get_logger(), "Received laser data");
   }
 
   void CollisionDetector::initParams(void)
   {
+    transformCoords_ = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    leftRangeY_ = {0.0, 0.0};
+    righRangeY_ = {0.0, 0.0};
+    frontRangeY_ = {0.0, 0.0};
+
     rate_ = (double)this->declare_parameter("node_rate", 2.0);
+    collisionDist_ = (double)this->declare_parameter("collision_distance", 0.3);
     laserTopic_ = (std::string)this->declare_parameter("laser_topic", "laser_scan");
-  }
-
-  int CollisionDetector::angle2laserIndex(float rad, const LaserScan & scanMsg)
-  {
-    double angle = rad;
-    /**
-     * Move 'angle' to a range [0 - 2PI]
-     */
-    while (angle >= (2.0 * M_PI)) {
-      angle -= (2.0 * M_PI);
-    }
-
-    size_t index = 0;
-    bool found = false;
-    while (index < scanMsg.ranges.size()) { 
-      if ((scanMsg.angle_min + ((float)index * scanMsg.angle_increment)) >= rad) {
-        found = true;
-        break;
-      }
-      index++;
-    }
-
-    if (found) {
-      return index;
-    } else {
-      return -1;
-    }
-
+    leftRangeY_ = (std::vector<double>)this->declare_parameter("left_range_y", leftRangeY_);
+    righRangeY_ = (std::vector<double>)this->declare_parameter("right_range_y", righRangeY_);
+    frontRangeY_ = (std::vector<double>)this->declare_parameter("front_range_y", frontRangeY_);
+    fixedFrame_ = (std::string)this->declare_parameter("fixed_frame", "base_scan");
+    childFrame_ = (std::string)this->declare_parameter("child_frame", "collision_frame");
+    transformCoords_ = (std::vector<double>)this->declare_parameter("transform_coords", transformCoords_);
   }
 
 } // end namespace collision_detector
